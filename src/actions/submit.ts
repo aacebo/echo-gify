479571425a2fb561ea33078952fe384537d6e560
aacebo <aacebowork@gmail.com>
2023-12-27 14:43:02 -0500

    Update submit.ts

--- src/actions/submit.ts
+++ src/actions/submit.ts
@@ -2,17 +2,6 @@ import { App, models } from '@aacebo/echo';
 
 export function submit(app: App) {
   return async ({ chat, user, value, ack }: { chat: models.Chat, user: models.User, value: any, ack: () => void }) => {
-    // await app.api.messages.createFor(user.name, chat.id, {
-    //   visibility: 'public',
-    //   child: {
-    //     type: 'container',
-    //     child: {
-    //       type: 'image',
-    //       url: value,
-    //     }
-    //   }
-    // });
-
     await app.api.views.chats.draft(user.name, chat.id, {
       child: {
         type: 'container',
