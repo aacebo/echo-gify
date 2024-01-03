f8ee6fe81961bab8677e9b5e069fa8c2a97f793a
aacebo <aacebowork@gmail.com>
2024-01-02 19:27:46 -0500

    Update link.ts

--- src/events/link.ts
+++ src/events/link.ts
@@ -3,9 +3,7 @@ import { App, Event } from '@aacebo/echo';
 export function link(app: App) {
   return async ({ event, ack }: { event: Event<'link'>, ack: () => void }) => {
     if (event.body.link.includes('.gif')) {
-      const message = await app.api.messages.getById(event.body.message_id);
-
-      await app.api.messages.extend(message.id, {
+      await app.api.messages.extend(event.body.message_id, {
         body: {
           type: 'container',
           child: {
